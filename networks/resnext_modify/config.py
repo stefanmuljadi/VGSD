5e6b7b0631e3cdd4485530ae81aeb812a6ffb257
= <stefan19052003@gmail.com>
2025-01-20 04:48:10 +0800

    wip

--- networks/resnext_modify/config.py
+++ networks/resnext_modify/config.py
@@ -1,4 +1,4 @@
 # resnext_101_32_path = 'resnext_101_32x4d.pth'
 
-resnext_101_32_path = '.../../checkpoints/resnext_101_32x4d.pth'
+resnext_101_32_path = '/content/VGSD/networks/resnext_modify/resnext_101_32x4d.pth'
 
